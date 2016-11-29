ff00c9084bf2d612f36807c9e31b628dd505a931
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-11-28 22:32:22 -0600

    render library list component with list items

---
+++ src/components/ListItem.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+import { CardSection } from './common/';
+
+class ListItem extends Component {
+  render() {
+    const { titleStyle } = styles;
+
+    return (
+      <CardSection>
+        <Text style={titleStyle}>
+          {this.props.library.title}
+        </Text>
+      </CardSection>
+    );
+  }
+}
+
+const styles = {
+  titleStyle: {
+    fontSize: 16,
+    paddingLeft: 15,
+    fontWeight: '700'
+  }
+};
+
+export default ListItem;
