94e8cff551940de29846a0cbcc552c96f01c28b8
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-11-28 20:36:33 -0600

    add reusable components and create redux store & reducers

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers/';
+import { Header } from './components/common/';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello React Native!</Text>
-    </View>
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Header headerText="Stacks" />
+      </View>
+    </Provider>
   );
 };
 
